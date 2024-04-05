cdbcd427a565acbdbc1345619444a8a3eb9373b9
Ruslan Bagymanov <ruslanbagymanov@gmail.com>
2024-04-05 21:14:36 +0300

    Stage 2 with tech task

---
+++ Server Stage 2/Client stage 2/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        try
+        {
+
+
+            await socket.ConnectAsync("127.0.0.1", 8888);
+            Console.WriteLine($"Подключение к {socket.RemoteEndPoint} установлено");
+
+
+            Console.WriteLine("Введите запрос (GET, PUT, DELETE)");
+            string action = Console.ReadLine().ToUpper();
+
+
+            if (action == "PUT")
+            {
+
+                Console.WriteLine("Введите файл с вашей папки:");
+                string name = Console.ReadLine();
+                string path = Path.Combine(@"C:\Users\rusla\client\data", name);
+
+
+                if (File.Exists(path))
+                {
+
+                    byte[] fileData = await File.ReadAllBytesAsync(path);
+                    byte[] lengthBytes = BitConverter.GetBytes(fileData.Length);
+
+                    Console.WriteLine("Введите название файла под которым будет сохранен файл на сервере:");
+                    string NewName = Console.ReadLine();
+
+
+                    if (NewName == "" || NewName == "\n")
+                    {
+                        string[] exm = name.Split('.');
+                        string path1 = @"C:\Users\rusla\client\data";
+                        NewName = GenerateUniqueFileName(path1, $"example.{exm[1]}");
+                    }
+
+
+                    byte[] data = System.Text.Encoding.UTF8.GetBytes($"{action},{NewName}");
+                    await socket.SendAsync(data, SocketFlags.None);
+
+
+                    using (FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                    {
+                        long fileSize = fileStream.Length;
+                        byte[] sizeBuffer = BitConverter.GetBytes(fileSize);
+                        await socket.SendAsync(sizeBuffer, SocketFlags.None);
+
+                        byte[] buffer = new byte[1024];
+                        int bytesRead;
+                        while ((bytesRead = await fileStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                        {
+                            await socket.SendAsync(buffer, SocketFlags.None);
+                        }
+                    }
+                }
+
+
+                else { Console.WriteLine("Такого файла нет на клиентском компьютере"); return; }
+            }
+
+
+
+            else if (action == "GET")
+            {
+                Console.WriteLine("Хотите получить файл по имени или ID?(ID, NAME):");
+                string By = Console.ReadLine().ToUpper();
+                if (By == "ID")
+                {
+                    Console.WriteLine("Введите ID");
+                    string id = Console.ReadLine();
+                    byte[] data = System.Text.Encoding.UTF8.GetBytes($"{action},{By},{id}");
+                    await socket.SendAsync(data, SocketFlags.None);
+                }
+                else if (By == "NAME")
+                {
+                    Console.WriteLine("Введите название файла с его расширением");
+                    string name = Console.ReadLine();
+                    byte[] data = System.Text.Encoding.UTF8.GetBytes($"{action},{By},{name}");
+                    await socket.SendAsync(data, SocketFlags.None);
+                }
+                Console.WriteLine("Запрос был отправлен на сервер!");
+            }
+
+            else if (action == "DELETE")
+            {
+                Console.WriteLine("Хотите удалить файл по имени или ID?(ID, NAME):");
+                string By = Console.ReadLine().ToUpper();
+                if (By == "ID")
+                {
+                    Console.WriteLine("Введите ID");
+                    string id = Console.ReadLine();
+                    byte[] data = System.Text.Encoding.UTF8.GetBytes($"{action},{By},{id}");
+                    await socket.SendAsync(data, SocketFlags.None);
+                }
+                else if (By == "NAME")
+                {
+                    Console.WriteLine("Введите название файла с его расширением");
+                    string name = Console.ReadLine();
+                    byte[] data = System.Text.Encoding.UTF8.GetBytes($"{action},{By},{name}");
+                    await socket.SendAsync(data, SocketFlags.None);
+                }
+                Console.WriteLine("Запрос был отправлен на сервер!");
+            }
+
+            else if (action == "EXIT")
+            {
+                await socket.SendAsync(Encoding.UTF8.GetBytes($"{action}"), SocketFlags.None);
+            }
+
+
+
+
+            if (action == "PUT")
+            {
+                byte[] buffer = new byte[1024];
+                int bytesReceived = await socket.ReceiveAsync(buffer, SocketFlags.None);
+                string[] result = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesReceived).Split(',');
+                if (result[0] == "200")
+                {
+                    Console.WriteLine("The response says that file was created!\t" + result[1]);
+                }
+                else if (result[0] == "403")
+                {
+                    Console.WriteLine("The response says that creating the file was forbidden!");
+                }
+            }
+
+
+
+
+
+            else if (action == "GET")
+            {
+                // Получаем строку от сервера
+                byte[] responseBytes = new byte[3]; // "200" занимает 3 байта
+                await socket.ReceiveAsync(responseBytes, SocketFlags.None);
+                string response = System.Text.Encoding.UTF8.GetString(responseBytes);
+
+                if (response == "200")
+                {
+
+                    Console.WriteLine("Введите название под которым сохранится файл:");
+                    string name = Console.ReadLine();
+                    byte[] lengthBytes = new byte[sizeof(int)];
+                    await socket.ReceiveAsync(lengthBytes, SocketFlags.None);
+                    int byteArrayLength = BitConverter.ToInt32(lengthBytes);
+
+                    // Получаем массив байтов от сервера
+                    byte[] buffer1 = new byte[byteArrayLength];
+                    await socket.ReceiveAsync(buffer1, SocketFlags.None);
+
+                    string path = $@"C:\Users\rusla\client\data\{name}";
+                    File.WriteAllBytes(path, buffer1);
+                    Console.WriteLine("Файл скачан к вам на компьютер");
+                }
+
+
+                else if (response == "404")
+                {
+                    Console.WriteLine("The response says that the file was not found!");
+                }
+            }
+
+
+
+
+            else if (action == "DELETE")
+            {
+                byte[] buffer = new byte[1024];
+                int bytesReceived = await socket.ReceiveAsync(buffer, SocketFlags.None);
+                string result = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+                if (result == "200")
+                {
+                    Console.WriteLine("The response says that file was deleted!");
+                }
+                else if (result == "404")
+                {
+                    Console.WriteLine("The response says that file not found!");
+                }
+            }
+
+        }
+        catch (SocketException)
+        {
+            Console.WriteLine($"Не удалось установить подключение с {socket.RemoteEndPoint}");
+        }
+    }
+    static string GenerateUniqueFileName(string directoryPath, string baseFileName)
+    {
+        string uniqueFileName = baseFileName;
+        int counter = 1;
+
+        // Получаем полный путь к файлу
+        string filePath = Path.Combine(directoryPath, uniqueFileName);
+
+        // Проверяем, существует ли файл с таким именем
+        while (File.Exists(filePath))
+        {
+            // Если файл существует, генерируем новое уникальное имя
+            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(baseFileName);
+            string fileExtension = Path.GetExtension(baseFileName);
+            uniqueFileName = $"{fileNameWithoutExtension}_{counter}{fileExtension}";
+
+            // Обновляем полный путь к файлу
+            filePath = Path.Combine(directoryPath, uniqueFileName);
+
+            counter++;
+        }
+
+        return uniqueFileName;
+    }
+}
---
+++ Server Stage 2/Server Stage 2/Program.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+class Program
+{   public static bool flag = true;
+    static async Task Main(string[] args)
+    {
+        string directoryPath = @"C:\Users\rusla\server\data";
+        string bumps = @"C:\Users\rusla\bumps\bumps.txt";
+
+        IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 8888);
+
+        Dictionary<string, string> bumpsfile = new Dictionary<string, string>();
+        string[] lines = File.ReadAllLines(bumps);
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(',');
+            if (parts.Length == 2)
+            {
+                bumpsfile.Add(parts[1], parts[0]);
+            }
+        }
+
+
+        using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        Console.WriteLine(ipPoint);
+        socket.Bind(ipPoint);
+        socket.Listen();
+        Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+        while (flag)
+        {
+            Socket client = await socket.AcceptAsync();
+            _ = Task.Run(() => ProcessClientAsync(client, directoryPath, bumpsfile, socket));
+        } 
+        
+       
+    }   
+
+    static async Task ProcessClientAsync(Socket client, string directoryPath, Dictionary<string,string> bumps,Socket socket)
+    {
+        try
+        {
+            Console.WriteLine($"Адрес подключенного клиента: {client.RemoteEndPoint}");
+
+            byte[] buffer = new byte[1024];
+            int bytesReceived = await client.ReceiveAsync(buffer, SocketFlags.None);
+            string data = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+            
+            Console.WriteLine($"Клиент передал запрос: {data}");
+
+            string[] request = data.Split(',');
+
+            if (request[0] == "PUT")
+            {
+                await HandlePutRequestAsync(client, request, directoryPath, bumps);
+            }
+            else if (request[0] == "GET")
+            {
+                await HandleGetRequestAsync(client, request, directoryPath,bumps);
+            }
+            else if (request[0] == "DELETE")
+            {
+                await HandleDeleteRequestAsync(client, request, directoryPath, bumps);
+            }
+            else if(data == "EXIT" || request[0] == "EXIT")
+            {
+                flag=false;
+                socket.Close();
+                StringBuilder sb = new StringBuilder();
+                foreach (var pair in bumps)
+                {
+                    sb.AppendLine($"{pair.Key},{pair.Value}");
+                }
+                File.WriteAllText(@"C:\Users\rusla\bumps\bumps.txt", sb.ToString());
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка обработки клиента: {ex.Message}");
+        }
+        finally
+        {
+            client.Close();
+        }
+    }
+
+    static async Task HandlePutRequestAsync(Socket client, string[] request, string directoryPath, Dictionary<string,string> bumps)
+    {
+        try
+        {
+            string fileName = request[1];
+            string filePath = Path.Combine(directoryPath, fileName);
+
+            byte[] lenghtBytes = new byte[sizeof(long)]; // Изменено на sizeof(long) для передачи размера файла
+            await client.ReceiveAsync(lenghtBytes, SocketFlags.None);
+            long dataSize = BitConverter.ToInt64(lenghtBytes); // Изменено на long для передачи размера файла
+
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                    long bytesReceived = 0;
+                    int chunkSize = 1024; // Размер части данных, которую будем принимать за один раз
+                    byte[] buffer = new byte[chunkSize];
+                    while (bytesReceived < dataSize)
+                    {
+                        int bytesRead = await client.ReceiveAsync(buffer, SocketFlags.None);
+                        await fileStream.WriteAsync(buffer, 0, bytesRead);
+                        bytesReceived += bytesRead;
+                    }
+            }
+
+            if (File.Exists(filePath))
+            {
+                
+                Guid guid = Guid.NewGuid();
+                bumps.Add(guid.ToString(),fileName);
+
+                byte[] otklik = System.Text.Encoding.UTF8.GetBytes($"200,{guid}");
+                await client.SendAsync(otklik, SocketFlags.None);
+            }
+            else
+            {
+                await client.SendAsync(Encoding.UTF8.GetBytes("403"), SocketFlags.None);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка обработки PUT запроса: {ex.Message}");
+        }
+    }
+
+    static async Task HandleGetRequestAsync(Socket client, string[] request, string directoryPath,Dictionary<string,string> bumps)
+    {
+        try
+        { string filem = request[2];
+            if (request[1] == "ID") { filem = FindById(request[2]); }
+            string filePath = Path.Combine(directoryPath, filem);
+            if (File.Exists(filePath))
+            {
+                byte[] bytes = await File.ReadAllBytesAsync(filePath);
+                byte[] lengthBytes = BitConverter.GetBytes(bytes.Length);
+
+                await client.SendAsync(Encoding.UTF8.GetBytes("200"), SocketFlags.None);
+                await client.SendAsync(lengthBytes, SocketFlags.None);
+                await client.SendAsync(bytes, SocketFlags.None);
+            }
+            else
+            {
+                await client.SendAsync(Encoding.UTF8.GetBytes("404"), SocketFlags.None);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка обработки GET запроса: {ex.Message}");
+        }
+    }
+
+    static async Task HandleDeleteRequestAsync(Socket client, string[] request, string directoryPath, Dictionary<string,string> bumps)
+    {
+        try
+        {   
+            string filem = request[2];
+            if (request[1] == "ID")
+            {
+                var item = bumps.FirstOrDefault(kvp => kvp.Value == filem);
+                filem = item.Key;
+            }
+                string path = Path.Combine(directoryPath,filem);
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                bumps.Remove(filem);
+                await client.SendAsync(Encoding.UTF8.GetBytes("200"), SocketFlags.None);
+            }
+            else
+            {
+                await client.SendAsync(Encoding.UTF8.GetBytes("404"), SocketFlags.None);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка обработки DELETE запроса: {ex.Message}");
+        }
+    }
+
+    static string FindById(string id)
+    {
+        string[] content = File.ReadAllLines(@"C:\Users\rusla\bumps\bumps.txt");
+        foreach (string line in content)
+        {
+            if (!string.IsNullOrEmpty(line))
+            {
+                string[] parts = line.Split(',');
+                if (parts[1] == id)
+                {
+                    return parts[0];
+                }
+            }
+        }
+        return "error";
+    }
+}
